ccf5c1ffb7b4734545487cae7bd17f3e66aee088
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-12-25 15:01:58 +0100

    Add a better message when 404

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,9 +5,27 @@ const PORT = 3000;
 
 const app = express();
 
-app.use('/edit', express.static(path.join(__dirname, "edit", "public")));
-app.use("/generate", express.static(path.join(__dirname, "generate", "public")));
+app.use("/edit", express.static(path.join(__dirname, "edit", "public")));
+app.use(
+  "/generate",
+  express.static(path.join(__dirname, "generate", "public"))
+);
+app.get("/", (req, res) => {
+  res.send(
+    `
+    <h1>Welcome to the dungeon project!</h1>
+    <ul>
+        <li>
+        Edit rooms at <a href="http://localhost:${PORT}/edit/">/edit/</a>
+        </li>
+        <li>
+        Generate dungeons at <a href="http://localhost:${PORT}/generate/">/generate/</a>
+        </li>
+    </ul>
+    `
+  );
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`);
+  console.log(`App listening at http://localhost:${PORT}`);
 });
