43e08f3e2da4200a1465fe7c042b950e3ed06ff5
Joel Thoms <jthoms+git@gmail.com>
2023-02-17 10:37:32 -0800

    remove global monkey patching of Math.random when seeded

--- src/generate/utils.ts
+++ src/generate/utils.ts
@@ -1,6 +1,25 @@
 import { TileMap } from "./types";
 
 /**
+ * An object containing random options that can be customized.
+ */
+export interface RandomOptions {
+  /**
+   * The seed used for generating random values.
+   */
+  seed: string | null;
+  random(): number;
+}
+
+/**
+ * Singleton configuration object for generating random options.
+ */
+export const randomOptions: RandomOptions = {
+  seed: null,
+  random: Math.random
+};
+
+/**
  * Create an empty tilemap.
  */
 export function createTilemap(
@@ -53,7 +72,7 @@ export function shuffleArray<T>(array: T[]): T[] {
   const newArray = [...array];
 
   for (let i = newArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(randomOptions.random() * (i + 1));
     [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
   }
 
@@ -64,14 +83,14 @@ export function shuffleArray<T>(array: T[]): T[] {
  * Generate a random number between `min` and `max`.
  */
 export function random(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(randomOptions.random() * (max - min + 1) + min);
 }
 
 /**
  * Return one of the values matching the randomly selected weight.
  */
 export function randomWeights<T>(weights: number[], values: T[]): T {
-  const num = Math.random();
+  const num = randomOptions.random();
   let s = 0;
   let lastIndex = weights.length - 1;
 
@@ -89,14 +108,14 @@ export function randomWeights<T>(weights: number[], values: T[]): T {
  * Return one of the values.
  */
 export function randomChoice<T>(values: T[]): T {
-  return values[Math.floor(Math.random() * values.length)];
+  return values[Math.floor(randomOptions.random() * values.length)];
 }
 
 /**
  * Return `true` if probability is matched.
  */
 export function randomProbability(probability: number): boolean {
-  return Math.random() > 1 - probability;
+  return randomOptions.random() > 1 - probability;
 }
 
 /**
